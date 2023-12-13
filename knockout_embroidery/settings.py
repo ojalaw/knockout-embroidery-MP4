665ffb43e338e4f6f429735c82ae4d4fcd655ef8
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-12-13 13:20:07 +0000

    Minor update to settings.py
    Made adjustments to facilitate django/heroku deployment

--- knockout_embroidery/settings.py
+++ knockout_embroidery/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d+c!0u$8o9=41-l$bwf^eh=$7hz-(6%*=b^dfn$*mur)at$f8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://knockout-embroidery-32949738fa37.herokuapp.com/']
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
