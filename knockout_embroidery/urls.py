5412a6e2fb3c611463b0c8b45e1a0e95e3c5c7a5
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-12-20 10:51:27 +0000

    Added logout function, minor update to settings.py and urls.py
    Added logout function to complement registration, login and now a logout function.

--- knockout_embroidery/urls.py
+++ knockout_embroidery/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('users/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
