1952294153aa8048bce54d504d43f37f443dc119
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-12-20 10:22:17 +0000

    Added login.html and added login url
    Added login page that users are redirected to after registering

--- knockout_embroidery/urls.py
+++ knockout_embroidery/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home.views import home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('users/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
