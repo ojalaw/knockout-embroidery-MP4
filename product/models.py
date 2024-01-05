a7c1ca105b2fef292335f2e1cd6d92218d4a874c
ojalaw <124128579+ojalaw@users.noreply.github.com>
2024-01-05 16:08:24 +0000

    Added product app which includes Product and Review models
    The product app will include all information on products available to users.

---
+++ product/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.CharField(max_length=10)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
