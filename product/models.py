db347689bebf1f21d823a5afbb1ff2cf982fa194
ojalaw <124128579+ojalaw@users.noreply.github.com>
2024-01-06 15:51:35 +0000

    Update to models.py
    Added colour field to products model

--- product/models.py
+++ product/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    colour = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.CharField(max_length=10)
     stock = models.IntegerField()
