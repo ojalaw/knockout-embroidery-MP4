30077f702789e4076b889171c3a2af9c84ffe89b
ojalaw <124128579+ojalaw@users.noreply.github.com>
2024-01-06 15:54:14 +0000

    Minor update to models.py
    Added default value to colour field in product model

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    colour = models.CharField(max_length=100)
+    colour = models.CharField(max_length=30, default='not specified')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.CharField(max_length=10)
     stock = models.IntegerField()
