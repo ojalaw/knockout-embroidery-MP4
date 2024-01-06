d0a0193cff9e052871768172e87b7b52e57035ff
ojalaw <124128579+ojalaw@users.noreply.github.com>
2024-01-06 16:27:56 +0000

    Minor update to products app
    Added more content to products model so that users can access the different products available to purchase.

--- product/models.py
+++ product/models.py
@@ -2,18 +2,36 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
+    SIZE_CHOICES = (
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'X-Large'),
+        ('XXL', 'XX-Large'),
+    )
+    COLOUR_CHOICES = (
+        ('Black', 'Black'),
+        ('White', 'White'),
+        ('Grey', 'Grey'),
+        ('Red', 'Red'),
+        ('Blue', 'Blue'),
+        ('Yellow', 'Yellow'),
+        ('Green', 'Green'),
+    )
+
     name = models.CharField(max_length=100)
     description = models.TextField()
-    colour = models.CharField(max_length=30, default='not specified')
+    colour = models.CharField(max_length=30, choices=COLOUR_CHOICES, default='not specified')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    size = models.CharField(max_length=10)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     image = models.ImageField(upload_to='product_images')
     
     def __str__(self):
         return self.name
-    
+
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
