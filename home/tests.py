9844e9baa57f4e12ea40561fe121e3c2b3044b62
ojalaw <124128579+ojalaw@users.noreply.github.com>
2024-01-27 15:19:15 +0000

    Added README images
    Added lighthouse testing images

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
-# Create your tests here.
+class BasicMathTest(TestCase):
+    def test_basic_math(self):
+        self.assertEqual(1 + 1, 2, "1 + 1 should equal 2")
